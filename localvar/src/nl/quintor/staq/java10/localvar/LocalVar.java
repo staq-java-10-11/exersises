06a5ab80f32a2f2bb4e50d36e610c6ff8edae37a
Milan Boers <mail@milanboers.nl>
2018-06-06 18:54:19 +0200

    Made exercises from localvar + integrated own exercises

--- localvar/src/nl/quintor/staq/java10/localvar/LocalVar.java
+++ localvar/src/nl/quintor/staq/java10/localvar/LocalVar.java
@@ -2,6 +2,7 @@ package nl.quintor.staq.java10.localvar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.DoubleBinaryOperator;
 import java.util.function.IntUnaryOperator;
 
@@ -9,21 +10,25 @@ import java.util.function.IntUnaryOperator;
 public class LocalVar {
 
     public void varWithoutInitializer() {
+        // Uncomment. Does it compile?
         //var myVariable;
         //myVariable = 2;
     }
 
     public void varWithGenerics() {
-        var myList = new ArrayList<String>();
+        // Replace type with var
+        List<String> myList = new ArrayList<String>();
         myList.add("Hello World");
+        // Uncomment. Does it compile? Why or why not? What is the type of "myList"?
         //myList.add(2);
     }
 
     public void varWithDiamond() {
-        var myList = new ArrayList<>();
+        // Replace type with var
+        List<String> myList = new ArrayList<>();
         myList.add("Hello World");
-        myList.add(2);
-        // Why does this compile?
+        // Uncomment. Does it compile? Why or why not?
+        //myList.add(2);
     }
 
     public void varInForLoop() {
@@ -33,52 +38,72 @@ public class LocalVar {
         }
     }
 
+    public void arrayInitialization() {
+        // Replace type with var. Does it compile?
+        String[] array2 = { "a", "b", "c" };
+    }
+
+    public void explicitArrayInitialization() {
+        // Replace type with var. Does it compile?
+        String[] array1 = new String[] { "a", "b", "c" };
+    }
+
     public void varReassignment() {
-        var myNumber = 3;
+        // Replace tpye with var. Does it compile?
+        int myNumber = 3;
         myNumber = 6;
         myNumber++;
     }
 
     public void varIsNotJavascript() {
         var myVariable = "Hello";
+        // Uncomment. Does it compile? Why or why not?
         //myVariable = 123;
     }
 
     public void initializeWithVar() {
         var myList = new ArrayList<String>();
-        var sameList = myList;
+        // Uncomment. Does it compile?
+        //var sameList = myList;
     }
 
     public void finalVar() {
         final var myValue = "Hello";
+        // Uncomment. Does it compile?
         //myValue = "World";
     }
 
     public void varLambda() {
+        // Uncomment. Does it compile?
         //var addOne = x -> x + 1;
     }
 
     public void varMethodReference() {
+        // Uncomment. Does it compile?
         //var toString = Object::toString;
     }
 
     public void varLambdaWithCast() {
-        var addOne = (IntUnaryOperator) x -> x + 1;
+        // Uncomment. Does it compile?
+        //var addOne = (IntUnaryOperator) x -> x + 1;
     }
 
     public void varInLambda() {
         DoubleBinaryOperator pythagoras = (a, b) -> {
-            var aSquared = a * a;
-            var bSquared = b * b;
+            // Replace both types with var. Does it compile?
+            double aSquared = a * a;
+            double bSquared = b * b;
             return Math.sqrt(aSquared + bSquared);
         };
     }
 
     public void varArgument(
+            // Uncomment. Does it compile?
             //var myArgument
     ) {
     }
 
+    // Uncomment. Does it compile?
     //public var returnVar() {
     //     return "Hello";
     //}
@@ -87,11 +112,27 @@ public class LocalVar {
         //IntUnaryOperator addOne = (var x) -> x + 1;
     }
 
+    // Rename this class to "var"
+    static class someclass {
+    }
+
     public void varIsNotAClass() {
         //Class cls = var.class;
     }
 
+    public void varIsNotAKeyword() {
+        // 1. Uncomment. Does it compile?
+        // 2. Replace "String" with "var". Keep the variable name the same. Does it compile?
+        //String var = "someString";
+    }
+
+    public void voidMethod() {
+        // Uncomment. Does it compile?
+        //var resultOfVoidMethod = voidMethod();
+    }
+
     static class VarMember {
+        // Uncomment. Does it compile?
         //var memberVariable = "Hello";
     }
 
@@ -99,19 +140,28 @@ public class LocalVar {
         Var var;
 
         Var var() {
+            // What is the type of the local variable "var"
             var var = this.var;
+            // What is the type of var.var?
             var.var = var();
             return var;
         }
     }
 
     public void anonymousClass() {
-        var anonymousVar = new Object() {
+        Object anonymousClass = new Object() {
             void myMethod() {
-                // Is this method reachable from `anonymousVar`?
             }
         };
-        anonymousVar.myMethod();
+        // Is myMethod reachable from `anonymousClass`? Uncomment.
+        // anonymousClass.myMethod();
+
+        var anonymousVar = new Object() {
+            void myMethod() {
+            }
+        };
+        // Is myMethod reachable from `anonymousVar`? Uncomment.
+        //anonymousVar.myMethod();
     }
 
     private interface Animal {
@@ -119,21 +169,26 @@ public class LocalVar {
     }
 
     private interface CanQuack {
-        default void quack() {
-            System.out.println("Quack!");
+        default String quack() {
+            return "Quack!";
         }
     }
 
     private interface CanWaggle {
-        default void waggle() {
-            System.out.println("Waggle waggle");
+        default String waggle() {
+            return "Waggle waggle";
         }
     }
 
     public void intersectionType() {
         var duck = (Animal & CanWaggle & CanQuack) () -> "Duck";
-        duck.getAnimalName(); // returns "Duck"
-        duck.quack(); // prints "Quack!"
-        duck.waggle(); // prints "Waggle waggle"
+        // What is the type of "duck"?
+
+        // Uncomment. Does it compile? What does this return?
+        //duck.getAnimalName();
+        // Uncomment. Does it compile? What does this return?
+        //duck.quack();
+        // Uncomment. Does it compile? What does this return?
+        //duck.waggle();
     }
 }
