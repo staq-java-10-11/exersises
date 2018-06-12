817289336416ac1a484eb222ac06cb8634d317a6
Bert Massop <b.massop@quintor.nl>
2018-06-12 14:22:01 +0200

    Split up HTTP exercises and add implementations

---
+++ httpclient-demo/src/nl/quintor/staq/java10/httpclient/http/ByteCountingExercise.java
@@ -0,0 +1,79 @@
+package nl.quintor.staq.java10.httpclient.http;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.Flow;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ByteCountingExercise {
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public long countBytes(final String url) {
+        // Make an asynchronous request to the url
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(url))
+                .build();
+
+        // Count the number of returned bytes, without persisting the entire response in-memory.
+        // Use HttpResponse.BodyHandler.fromSubscriber to wire up your ByteCountingSubscriber as a BodyHandler, and let
+        // obtain the "body" from the Subscriber's byte count.
+        var responseFuture = httpClient.sendAsync(httpRequest,
+                HttpResponse.BodyHandler.fromSubscriber(new ByteCountingSubscriber(), ByteCountingSubscriber::getReceivedByteCount)
+        );
+        var response = responseFuture.join();
+        System.out.println("ByteCountingExercise is done -  status was " + response.statusCode() + "; response reported " + response.body() + " bytes");
+        return response.body();
+    }
+
+    /**
+     * A Flow Subscriber that counts the number of bytes it receives.
+     */
+    static class ByteCountingSubscriber implements Flow.Subscriber<List<ByteBuffer>> {
+
+        private Flow.Subscription subscription;
+        private final AtomicLong received = new AtomicLong();
+
+        long getReceivedByteCount() {
+            // 1. Return the number of bytes received so far.
+            return received.get();
+        }
+
+        @Override
+        public void onSubscribe(Flow.Subscription subscription) {
+            this.subscription = subscription;
+            // What happens if you remove this line?
+            subscription.request(1);
+        }
+
+        @Override
+        public void onNext(List<ByteBuffer> item) {
+            // 2. Count the number of received bytes.
+            // Hint: the {@code remaining()} method on ByteBuffer gives the number of bytes in the buffer.
+            final long bytes = item.stream().mapToLong(ByteBuffer::remaining).sum();
+            received.addAndGet(bytes);
+
+            System.out.println("ByteCountingSubscriber received " + bytes + " bytes");
+            // What happens if you remove this line?
+            subscription.request(1);
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            System.out.println("ByteCountingSubscriber received an error: " + throwable.toString());
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println("ByteCountingSubscriber completed after " + received + " bytes");
+        }
+    }
+}
---
+++ httpclient-demo/src/nl/quintor/staq/java10/httpclient/http/ServerPushExercise.java
@@ -0,0 +1,56 @@
+package nl.quintor.staq.java10.httpclient.http;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class ServerPushExercise {
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public CompletableFuture<ByteCountingExercise.BodyAndPushed> serverPushResponses(final String url) {
+        // 1. Make an asynchronous request to "url", and return a future of a BodyAndPushed object, where "body" is
+        // the body of the page returned, and "pushed" are the pushed resources as a map from url to body as a byte
+        // array.
+        // Hint: use MultiSubscriber.asMap
+        // Hint: do not wait forever for futures. Make them time out and discard them. The future should complete when
+        // all responses have either been completed or have timed out.
+        // BONUS: Can you implement your own MultiSubscriber to do this more efficiently?
+        var httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+        var multiSubscriber = HttpResponse.MultiSubscriber.asMap(
+                r -> Optional.of(HttpResponse.BodyHandler.asByteArray()),
+                false
+        );
+        var responseFuture = httpClient.sendAsync(httpRequest, multiSubscriber);
+
+        var bodyFuture = responseFuture.thenCompose(map -> map.get(httpRequest)).thenApply(HttpResponse::body);
+        var pushedFuture = responseFuture.thenApply(map -> map.entrySet().stream()
+                .filter(entry -> !entry.getKey().equals(httpRequest))
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().uri().getPath(),
+                        entry -> entry.getValue().completeOnTimeout(null, 3, TimeUnit.SECONDS)
+                )));
+
+        var allFutures = responseFuture.thenCompose(v ->
+                CompletableFuture.allOf(pushedFuture.join().values().toArray(new CompletableFuture[0])));
+
+        return allFutures.thenApply(v -> {
+            var body = bodyFuture.join();
+            var pushedFutures = pushedFuture.join();
+            var pushed = pushedFutures.entrySet().stream()
+                    .filter(entry -> entry.getValue().join() != null)
+                    .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().join().body()));
+            var bodyAndPushed = new ByteCountingExercise.BodyAndPushed();
+            bodyAndPushed.body = new String(body);
+            bodyAndPushed.pushed = pushed;
+            return bodyAndPushed;
+        });
+    }
+}
